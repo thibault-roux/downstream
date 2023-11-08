204bdb8716a4a6566c74d67bb6136edea69ac3d7
thibault-roux <thibault.roux1@laposte.net>
2023-11-08 18:39:36 +0100

    Fix bugs

--- correlation.py
+++ correlation.py
@@ -1,6 +1,8 @@
 from deep_translator import GoogleTranslator as Translator
 import progressbar
 
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 def read_hats():
     # dataset = [{"reference": ref, "hypA": hypA, "nbrA": nbrA, "hypB": hypB, "nbrB": nbrB}, ...]
@@ -10,7 +12,7 @@ def read_hats():
         for line in file:
             line = line[:-1].split("\t")
             dictionary = dict()
-            dictionary["reference"] = line[0]
+            dictionary["ref"] = line[0]
             dictionary["hypA"] = line[1]
             dictionary["nbrA"] = int(line[2])
             dictionary["hypB"] = line[3]
@@ -27,7 +29,7 @@ def translate_hats_and_save(dataset, verbose=True):
         bar.start()
         i = 0
     for dictionary in dataset:
-        ref = dictionary["reference"]
+        ref = dictionary["ref"]
         hypA = dictionary["hypA"]
         hypB = dictionary["hypB"]
         tradref = translator.translate(ref)
@@ -49,7 +51,7 @@ def load_translated_hats():
         for line in file:
             line = line[:-1].split("\t")
             dictionary = dict()
-            dictionary["reference"] = line[0]
+            dictionary["ref"] = line[0]
             dictionary["hyp"] = line[1]
             dictionary["tradref"] = line[2]
             dictionary["tradhyp"] = line[3]
@@ -67,14 +69,13 @@ def semdist(ref, hyp, memory):
 def bertscore(ref, hyp, memory):
     scorer = memory
     P, R, F1 = scorer.score([hyp], [ref])
-    return 100-F1*100
+    return 100-F1.numpy()[0]*100 # lower is better
 
 def save_semdist_bertscore(verbose=True):
     # hats must have been translated
 
     # SemDist Sentence Camembert-large
     from sentence_transformers import SentenceTransformer
-    from sklearn.metrics.pairwise import cosine_similarity
     semdist_model = SentenceTransformer('dangvantuan/sentence-camembert-large')
 
     # BERTScore
