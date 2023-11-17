0b255a2df09bf42eee1a5905b8a103992f296f40
thibault-roux <thibault.roux1@laposte.net>
2023-11-17 09:58:41 +0100

    Alignment with three inputs

---
+++ triple_alignment.py
@@ -0,0 +1,44 @@
+def levenshtein_distance(str1, str2):
+    m, n = len(str1), len(str2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+
+    return dp[m][n]
+
+def triple_alignment(str1, str2, str3):
+    len1, len2, len3 = len(str1.split()), len(str2.split()), len(str3.split())
+    dp = [[[0] * (len3 + 1) for _ in range(len2 + 1)] for _ in range(len1 + 1)]
+
+    for i in range(len1 + 1):
+        for j in range(len2 + 1):
+            for k in range(len3 + 1):
+                if i == 0 or j == 0 or k == 0:
+                    dp[i][j][k] = i + j + k
+                else:
+                    cost = 0 if str1.split()[i - 1] == str2.split()[j - 1] == str3.split()[k - 1] else 1
+                    dp[i][j][k] = min(
+                        dp[i - 1][j][k] + 1,
+                        dp[i][j - 1][k] + 1,
+                        dp[i][j][k - 1] + 1,
+                        dp[i - 1][j - 1][k - 1] + cost
+                    )
+
+    return dp[len1][len2][len3]
+
+# Exemple d'utilisation
+str1 = "salut tu vas bien car pas moi"
+str2 = "salu tu vax sien car pa moi"
+str3 = "euh salu tu va viens car pas"
+
+distance = triple_alignment(str1, str2, str3)
+print(f"Distance d'alignement triple de Levenshtein : {distance}")
