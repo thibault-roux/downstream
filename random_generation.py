93e09a522fb863d98460b64e54378311c91fbe50
thibault-roux <thibault.roux1@laposte.net>
2023-11-17 13:21:58 +0100

    Working by random unique modification

--- random_generation.py
+++ random_generation.py
@@ -14,7 +14,7 @@ import jiwer
 def WE(ref, hyp):
     return jiwer.wer(ref, hyp)*len(ref.split(" "))
 
-def infer_hypothesis(ref, hypA, hypB):
+def infer_hypothesis1(ref, hypA, hypB):
     # my method is to generate random ancestors until one is correct
     # the condition is that the ancestor must have one error in common with each child, i.e WE(ancestor, child) = 1
 
@@ -52,8 +52,59 @@ def infer_hypothesis(ref, hypA, hypB):
             return ancestor
         limit += 1
 
+
+
+def infer_hypothesis2(ref, hypA, hypB):
+    # instead of fully generate the hypothesis, I could start from hypothesis A and B and generate a random ancestor
+
+    vocabulary_save = []
+    for word in ref.split(" "):
+        vocabulary_save.append(word)
+    for word in hypA.split(" "):
+        vocabulary_save.append(word)
+    for word in hypB.split(" "):
+        vocabulary_save.append(word)
+
+    limit = 0
+    while limit < 10000:
+        hyp = hypA.split(" ")
+        # randomly delete a word, insert a word from vocabulary or substitute a word from vocabulary
+        # randomly choose an operation
+        operation = random.choice(["delete", "insert", "substitute"])
+        # randomly choose a word from vocabulary
+        word = random.choice(vocabulary_save)
+        # randomly choose a position
+        position = random.randint(0, len(hyp)-1)
+        # apply operation
+        if operation == "delete":
+            hyp.pop(position)
+        elif operation == "insert":
+            hyp.insert(position, word)
+        elif operation == "substitute":
+            hyp[position] = word
+        hyp = " ".join(hyp)
+        # check if it is correct
+        hyp_hypA = WE(hyp, hypA)
+        hyp_hypB = WE(hyp, hypB)
+        hyp_ref = WE(hyp, ref)
+        ref_hypA = WE(ref, hypA)
+        ref_hypB = WE(ref, hypB)
+        if hyp_hypA == 1 and hyp_hypB == 1 and ref_hypA + 1 == hyp_ref and ref_hypB + 1 == hyp_ref:
+            print(limit)
+            return hyp
+        limit += 1
+
+
 if __name__ == "__main__":
-    ref = "salut tu vas bien"
-    hypA = "salu tu va bien ouais"
-    hypB = "salu tu vas viens ouais"
-    print(infer_hypothesis(ref, hypA, hypB))
+    # ref = "salut tu vas bien"
+    # hypA = "salu tu va bien ouais"
+    # hypB = "salu tu vas viens ouais"
+    
+    ref = "salut tu vas bien car pas moi"
+    hypA = "salu tu vas sien car pa moi"
+    hypB = "salu tu va sien car pas moi"
+
+    # ref = "A B C D"
+    # hypA = "A B E D"
+    # hypB = "A B C F"
+    print(infer_hypothesis2(ref, hypA, hypB))
