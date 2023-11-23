bb9cf4af79508191ba864f2d61d3ca460fac1a4a
thibault-roux <thibault.roux1@laposte.net>
2023-11-23 16:06:30 +0100

    Coqui modifs

--- tts/generate2.py
+++ tts/generate2.py
@@ -1,9 +1,10 @@
 import torch
 from TTS.api import TTS
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Init TTS with the target model name
-tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False).to(device)
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
 # Run TTS
-tts.tts_to_file("C'est le clonage de la voix.", language="fr-fr", file_path="files/techno.wav")
+tts.tts_to_file("C'est le clonage de la voix.", speaker_wav="files/bfm.wav", language="fr-fr", file_path="files/techno.wav")
