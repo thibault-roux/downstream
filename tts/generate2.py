fb98d43621b1408b90d231421d315aedb75cf3f6
thibault-roux <thibault.roux1@laposte.net>
2023-11-23 17:37:07 +0100

    Working method

--- tts/generate2.py
+++ tts/generate2.py
@@ -6,5 +6,10 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Init TTS with the target model name
 tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
-# Run TTS
-tts.tts_to_file("C'est le clonage de la voix.", speaker_wav="files/bfm.wav", language="fr-fr", file_path="files/techno.wav")
+
+multiples_texts = ["salut tu vas bien", "oui tu vas bien", "c'est super mon ami", "content de te voir", "également"]
+
+i = 0
+for text in multiples_texts:
+    # Run TTS
+    tts.tts_to_file(text, speaker_wav="files" + str(i) + "/bfm.wav", language="fr", file_path="files/techno.wav")
