0596fdbc5be01e74a9c7035a3ccb4f71051743b8
thibault-roux <thibault.roux1@laposte.net>
2023-12-12 22:25:04 +0100

    Finder complete

--- datasets/find_example.py
+++ datasets/find_example.py
@@ -1,5 +1,12 @@
-
+import jiwer
 
 with open("corrections_traduction_semdist_bertscore.txt", "r", encoding="utf8") as file:
     for line in file:
         line = line.split("\t")
+        ref = line[0]
+        hyp = line[1]
+        if len(ref.split(" ")) < 7 and len(hyp.split(" ")) < 7 and jiwer.wer(ref, hyp)*len(ref.split(" ")) == 3:
+            print(ref)
+            print(hyp)
+            input()
+
